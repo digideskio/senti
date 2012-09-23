81e736875e05e65c9b0fca433ae69e14072b7596
John Roa <johnroa@gmail.com>
2012-09-23 15:10:36 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Senti::Application.config.secret_token = 'f12497274826183fe78cb961e2ca2dee2976c7e557a070a809f93eb5a59c2c7a9ce44ae416fa0fa226e73ad14bc11483349172945dbead529db9709bbfa177f6'
